7d357bc190555579ef344a1b6b7e3f3ed628317d
jabrock76 <jamiebrock0910@gmail.com>
2026-05-26 19:14:13 -0400

    Updates News Items

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,117 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../../css/styles/newItemsSlider.css";
+
+//Custom Arrows for the slider
+const NextArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="custom-arrow custom-next" onClick={onClick}>
+      <i className="fa fa-angle-right"></i>
+    </div>
+  );
+};
+ 
+const PrevArrow = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="custom-arrow custom-prev" onClick={onClick}>
+      <i className="fa fa-angle-left"></i>
+    </div>
+  );
+};
 
 const NewItems = () => {
+
+   const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const getTimeLeft = (expiryDate) => {
+    const difference = expiryDate - Date.now();
+    if (difference <= 0) return "Expired";
+
+    const hours = Math.floor(difference / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+    return `${hours}h ${minutes}m ${seconds}s`;
+  };
+
+  const [timeLeft, setTimeLeft] = useState({});
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const updatedTimes = {};
+      data.forEach((item) => {
+        updatedTimes[item.id] = getTimeLeft(item.expiryDate);
+      });
+      setTimeLeft(updatedTimes);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [data]);
+  
+    useEffect(() => { 
+      const fetchData = async () => {
+        try {
+          const response = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems");
+          const jsonData = response.data;
+          setData(jsonData);
+        } catch (error) {
+          setError(error);
+        } finally {
+          setLoading(false);
+        }
+    };
+    fetchData();
+  }, []);
+  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  
+  //  Slider settings with responsive breakpoints
+  const settings = {
+    infinite: true,
+    speed: 300, 
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,  
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        } 
+      }
+    ]
+  }
+
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,21 +122,24 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          <Slider {...settings}>
+            {data.map((item, index) => (
+            <div key={index} style={{ padding: " 0 10px"}}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
                     to="/author"
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
-                    title="Creator: Monica Lucas"
+                    title={`Creator: ${item.authorName}`}
                   >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={item.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
-                <div className="de_countdown">5h 30m 32s</div>
+                <div className="de_countdown">
+                  {timeLeft[item.id] || getTimeLeft(item.expiryDate)}
+                </div>
 
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
@@ -36,40 +147,42 @@ const NewItems = () => {
                       <button>Buy Now</button>
                       <div className="nft__item_share">
                         <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
+                        <a href="#" target="_blank" rel="noreferrer">
                           <i className="fa fa-facebook fa-lg"></i>
                         </a>
                         <a href="" target="_blank" rel="noreferrer">
                           <i className="fa fa-twitter fa-lg"></i>
                         </a>
-                        <a href="">
+                        <a href="#">
                           <i className="fa fa-envelope fa-lg"></i>
                         </a>
                       </div>
                     </div>
                   </div>
 
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
+                      src={item.nftImage}
+                      className="nft__item_preview"
                       alt=""
+                      style={{ width: "100%", height: "200px", display: "block", objectFit: "cover" }}
                     />
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
             </div>
           ))}
+          </Slider>
         </div>
       </div>
     </section>
